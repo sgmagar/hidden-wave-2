fb2ed051b547d63a31d92f76b11ffb524d2191e7
Saurav <sp.gharti@gmail.com>
2023-03-29 14:04:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-wave-2",
+  url: "https://hidden-wave-2.botics.co",
+  api: "https://hidden-wave-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
